f39e7b3fd132a72fc2234caf67b0b95fa0e3af69
JuanPh-02 <juanph02@outlook.com>
2025-07-26 14:24:36 -0500

    funcion retirarVehiculo

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function mostrarMenu() {
     console.log('\n SISTEMA DE PARQUEADERO');
     console.log("1. Registar vehiculo");
     console.log("2. Mostrar vehiculos");
-    console.log("3. Retirar vehiculo");
+    console.log("3. Retirar vehiculo"); 
     console.log("4. SALIR");
 
     const opcion = readline.question("Ingrese una opcion: ");
@@ -33,6 +33,8 @@ function mostrarMenu() {
             break;
         case "3":
             // RETIRAR VEHICULO
+            const placa = readline.question("Ingrese la placa del vehiculo a retirar: ")
+            retirarVehiculo(placa);
             break;
         case "4":
             // SALIR
@@ -57,6 +59,35 @@ function registrarVehiculo(placa, tipo) {
     console.log("El vehiculo fue registrado correctamente");
 }
 
+function retirarVehiculo(placa) {
+    const indiceVehiculo = parqueadero.findIndex((vehiculo) => vehiculo.placa === placa);
+
+    if (indiceVehiculo === -1) {
+        console.log("Vehiculo no encontrado");
+        return;
+    }
+
+    const vehiculo = parqueadero[indiceVehiculo];
+    const horaSalida = Date.now();
+
+    const tiempoMs = horaSalida - vehiculo.horaEntrada;
+    const tiempoEnHoras = tiempoMs / (1000 * 60 * 60)
+    const tiempoRedondeado = Math.ceil(tiempoEnHoras)
+
+    let tarifaAPagar = 0;
+    if (vehiculo.tipo === "carro") {
+        tarifaAPagar = tiempoRedondeado * 2000;
+    } else {
+        tarifaAPagar = tiempoRedondeado * 1000;
+    }
+
+    parqueadero.splice(indiceVehiculo, 1);
+
+    console.log(`El vehiculo con placa ${placa} fue retirado.`);
+    console.log(`Tiempo en el parqueadero: ${tiempoRedondeado} horas`);
+    console.log(`Total a pagar: ${tarifaAPagar}`);
+}
+
 function mostrarVehiculos() {
     if(parqueadero.length === 0) {
         console.log("No hay vehiculos en el parqueadero.");
@@ -67,11 +98,4 @@ function mostrarVehiculos() {
     parqueadero.forEach((vehiculo, i) => console.log(`${i + 1}. Placa: ${vehiculo.placa} - Tipo: ${vehiculo.tipo} - Hora Entrada: ${new Date(vehiculo.horaEntrada).toLocaleTimeString()} `))
 }
 
-
 mostrarMenu();
-const horaEnMs = Date.now();
-let fechaEntrada = new Date(horaEnMs)
-fechaEntrada.toLocaleTimeString();
-// const horaEntrada = new Date();
-// console.log(horaEntrada.toLocaleTimeString());
-// console.log(horaEntrada.)
